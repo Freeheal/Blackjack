3afcfe87dff1ef923c8962567e9e278ee1907bbf
Freeheal <lucaskoehler50@gmail.com>
2026-06-12 12:34:57 +0200

    Update Player.java

--- core/src/main/java/io/github/some_example_name/Player.java
+++ core/src/main/java/io/github/some_example_name/Player.java
@@ -1,5 +1,12 @@
 package io.github.some_example_name;
 
 public class Player {
+	private int geld;
+	private String name;
+	private int streak;
+
+	public Player(String name) {
+		this.name = name;
+	}
 
 }
